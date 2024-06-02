7a7c9430df310367266a0fce5ba66afc7cd4ec7d
SJshubzz <shubhamjadhav4899@gmail.com>
2024-06-02 18:16:52 +0530

    initial commit

---
+++ src/main/java/com/docguard/DocguardApplication.java
@@ -0,0 +1,13 @@
+package com.docguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docguard/DocguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
